eefea5ebacba0fbbe6c04b15bed436d6adbfdac0
Harsh <parmarharsh6480@gmail.com>
2025-09-16 08:29:01 +0530

    Add Mysql DB connection.

--- src/main/java/com/harsh/lessonswithai/LessonswithaiApplication.java
+++ src/main/java/com/harsh/lessonswithai/LessonswithaiApplication.java
@@ -2,7 +2,10 @@ package com.harsh.lessonswithai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class LessonswithaiApplication {
 
@@ -10,4 +13,8 @@ public class LessonswithaiApplication {
         SpringApplication.run(LessonswithaiApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String homePage(){
+        return "Hello World";
+    }
 }
