58943558188eccad966f110044954041841c135c
Harsh <parmarharsh6480@gmail.com>
2025-09-16 08:06:28 +0530

    Project Init

---
+++ src/main/java/com/harsh/lessonswithai/LessonswithaiApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.lessonswithai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonswithaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonswithaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harsh/lessonswithai/LessonswithaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.lessonswithai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonswithaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
