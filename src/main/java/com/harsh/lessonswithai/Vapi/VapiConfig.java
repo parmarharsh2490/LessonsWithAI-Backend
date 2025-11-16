a8d3d8af70c0f378d406bf4696f802c21076b7d5
Harsh <parmarharsh6480@gmail.com>
2025-11-16 19:42:38 +0530

    Update VapiConfig

--- src/main/java/com/harsh/lessonswithai/Vapi/VapiConfig.java
+++ src/main/java/com/harsh/lessonswithai/Vapi/VapiConfig.java
@@ -4,37 +4,29 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.util.Objects;
 
 @Getter
 @Setter
 @Slf4j
 @Service
 public class VapiConfig {
-    @Value("${spring.application.vapiApiKey}")
-    private String vapiApiKey;
+    @Value("${spring.application.vapi_private_key}")
+    private String vapiPrivateKey;
 
     private final String vapiURL = "https://api.vapi.ai/";
 
     public OkHttpClient getClient() {
         return new OkHttpClient.Builder()
-                .addInterceptor(new Interceptor() {
-                    @NotNull
-                    @Override
-                    public Response intercept(@NotNull Chain chain) throws IOException {
-                        Request original = chain.request();
-                        Request newRequest = original.newBuilder()
-                                .addHeader("Content-Type", "application/json")
-                                .addHeader("Authorization", "Bearer " + vapiApiKey)
-                                .build();
+                .addInterceptor(chain -> {
+                    Request original = chain.request();
+                    Request newRequest = original.newBuilder()
+                            .addHeader("Content-Type", "application/json")
+                            .addHeader("Authorization", "Bearer " + vapiPrivateKey)
+                            .build();
 
-                        return chain.proceed(newRequest);
-                    }
+                    return chain.proceed(newRequest);
                 })
                 .build();
     }
